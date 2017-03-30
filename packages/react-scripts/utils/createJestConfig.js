58e5150f94a2a93694c383050f3a313965af5a88
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2017-03-30 11:39:55 +0200

    fixed typo

--- packages/react-scripts/utils/createJestConfig.js
+++ packages/react-scripts/utils/createJestConfig.js
@@ -44,7 +44,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
         '<rootDir>/node_modules/babel-jest'
         : resolve('config/jest/babelTransform.js'),
       '^.+\\.(ts|tsx)$': isEjecting ?
-        '<rootDir>/config/jest/tsTransform.js",'
+        '<rootDir>/config/jest/tsTransform.js'
         : resolve('config/jest/tsTransform.js'),
       '^.+\\.css$': resolve('config/jest/cssTransform.js'),
       '^(?!.*\\.(js|jsx|css|json)$)': resolve('config/jest/fileTransform.js'),
